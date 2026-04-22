2afeaa53e5f76fa0ae2369799f6cf6491d27b0f1
Aijá Gourgel <agourgel44@gmail.com>
2026-04-22 10:35:22 +0200

    BODY - Update - Efeito scrollspy com js

--- StarBootstrap/script.js
+++ StarBootstrap/script.js
@@ -12,3 +12,26 @@ window.addEventListener('scroll', () => {
 
 
 
+const sections = document.querySelectorAll("section");
+const navLinks = document.querySelectorAll("#main-nav .nav-link");
+
+window.addEventListener("scroll", () => {
+    let current = "";
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 150; // compensação da navbar
+        const sectionHeight = section.clientHeight;
+
+        if (window.scrollY >= sectionTop) {
+            current = section.getAttribute("id");
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove("active");
+
+        if (link.getAttribute("href") === "#" + current) {
+            link.classList.add("active");
+        }
+    });
+});
